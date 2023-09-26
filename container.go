847fa05a7c09b9ceaf23364553f1c56936f8d4bf
Ivan Ivković <ivkovic.ivan@outlook.com>
2023-09-26 22:47:31 +0200

    Singleton and transient configurations added.

--- container.go
+++ container.go
@@ -1,11 +1,12 @@
 package falconsnest
 
 import (
-	"fmt"
+	"falconsnest/internal/configuration"
 	"reflect"
 )
 
 type Container interface {
+	addConfiguration(t reflect.Type, config configuration.Configuration)
 }
 
 type container struct {
@@ -17,22 +18,10 @@ func GetContainer() Container {
 	return c
 }
 
+func (c container) addConfiguration(t reflect.Type, config configuration.Configuration) {
+
+}
+
 func Register[I any, S any]() Registrar[I, S] {
-	var i [0]I
-	var s [0]S
-	var it = reflect.TypeOf(i).Elem()
-	var st = reflect.TypeOf(s).Elem()
-	var ik = it.Kind()
-	var sk = st.Kind()
-
-	if ik != reflect.Interface || sk != reflect.Struct {
-		panic(newRegistrationError("Interface and struct expected as type parameters."))
-	}
-
-	if !st.Implements(it) {
-		var msg = fmt.Sprintf("%s does not implement %s.", st.Name(), it.Name())
-		panic(newRegistrationError(msg))
-	}
-
-	return newRegistrar[I, S]()
+	return newRegistrar[I, S](c)
 }
--- registrar.go
+++ registrar.go
@@ -1,21 +1,56 @@
 package falconsnest
 
+import (
+	"falconsnest/internal/configuration"
+	"fmt"
+	"reflect"
+)
+
 type Registrar[I any, S any] interface {
 	AsSingleton()
 	AsTransient()
 }
 
-func newRegistrar[I any, S any]() Registrar[I, S] {
-	return registrar[I, S]{}
+type registrar[I any, S any] struct {
+	container Container
 }
 
-type registrar[I any, S any] struct {
+func newRegistrar[I any, S any](c Container) Registrar[I, S] {
+	return registrar[I, S]{
+		container: c,
+	}
 }
 
 func (r registrar[I, S]) AsSingleton() {
+	interfaceType, implementingType := r.validateTypes[I, S]()
+	config := configuration.NewSingletonConfiguration(interfaceType, implementingType)
 
+	r.container.addConfiguration(interfaceType, config)
 }
 
 func (r registrar[I, S]) AsTransient() {
+	interfaceType, implementingType := r.validateTypes[I, S]()
+	config := configuration.NewTransientConfiguration(interfaceType, implementingType)
 
+	r.container.addConfiguration(interfaceType, config)
+}
+
+func (r registrar[I, S]) validateTypes() (reflect.Type, reflect.Type) {
+	var i [0]I
+	var s [0]S
+	var it = reflect.TypeOf(i).Elem()
+	var st = reflect.TypeOf(s).Elem()
+	var ik = it.Kind()
+	var sk = st.Kind()
+
+	if ik != reflect.Interface || sk != reflect.Struct {
+		panic(newRegistrationError("Interface and struct expected as type parameters."))
+	}
+
+	if !st.Implements(it) {
+		var msg = fmt.Sprintf("%s does not implement %s.", st.Name(), it.Name())
+		panic(newRegistrationError(msg))
+	}
+
+	return it, st
 }
